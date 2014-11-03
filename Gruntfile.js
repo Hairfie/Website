2c4c869a0e1676ef892b1f85ef4051e62b8ebe01
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-11-03 17:23:14 +0100

    setup grunt and front

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var request = require('request');
+
+module.exports = function (grunt) {
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+
+  var reloadPort = 35729, files;
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    develop: {
+      server: {
+        file: 'bin/www'
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'public/css/style.css' : 'public/scss/style.scss'
+        }
+      }
+    },
+    watch: {
+      options: {
+        nospawn: true,
+        livereload: reloadPort
+      },
+      js: {
+        files: [
+          'app.js',
+          'lib/**/*.js',
+          'services/**/*.js',
+          'config/*.js'
+        ],
+        tasks: ['develop', 'delayed-livereload']
+      },
+      views: {
+        files: [
+          'views/*.ejs',
+          'views/**/*.ejs'
+        ],
+        options: { livereload: reloadPort }
+      },
+      css: {
+        files: 'public/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.config.requires('watch.js.files');
+  files = grunt.config('watch.js.files');
+  files = grunt.file.expand(files);
+
+  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
+    var done = this.async();
+    setTimeout(function () {
+      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function(err, res) {
+          var reloaded = !err && res.statusCode === 200;
+          if (reloaded)
+            grunt.log.ok('Delayed live reload successful.');
+          else
+            grunt.log.error('Unable to make a delayed live reload.');
+          done(reloaded);
+        });
+    }, 500);
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('default', ['develop', 'watch', 'sass']);
+};
