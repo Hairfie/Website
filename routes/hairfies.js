9c9b980d2876dde0033e990d335729de246254a9
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-11-18 19:25:45 +0100

    fix somethings

--- routes/hairfies.js
+++ routes/hairfies.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('underscore');
 
 var hairfie = require('../services/hairfie.js');
 var metaGenerator = require('../services/metaGenerator.js');
+
+var descriptionsGenerator = function(hairfie) {
+    var descriptions, tags = '', oldDescription = '', businessName = '';
+    if(hairfie.tags) {
+        tags = _.map(hairfie.tags, function(tag) { return '#'+tag.name.replace(/ /g,''); }).join(" ");
+    }
+    if(hairfie.description) {
+        oldDescription = ' ' + hairfie.description;
+    }
+    if(hairfie.business) {
+        businessName = ' made at ' + hairfie.business.name;
+    }
+    descriptions = {
+        twitter: encodeURIComponent(tags + oldDescription + businessName + ' #hairfie'),
+        facebook: tags + oldDescription + businessName,
+        display: tags + oldDescription
+    };
+
+    return descriptions;
+};
 
 router.get('/:id', function(req, res) {
     hairfie.getHairfie(req.params.id)
@@ -11,6 +32,7 @@ router.get('/:id', function(req, res) {
                 res.status(404);
                 res.send('Hairfie not found');
             } else {
+                hairfie.descriptions = descriptionsGenerator(hairfie);
                 metaGenerator.getHairfieMetas(hairfie, function(metas) {
                     res.render('hairfies/show', {
                         hairfie: hairfie,
