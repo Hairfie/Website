76575ff0a69705157d49ab5bc921bc32719f9f0b
Antoine Hérault <antoine.herault@gmail.com>
2014-11-26 09:08:58 +0100

    Add missing handler

--- client/components/ShowBusinessPage.jsx
+++ client/components/ShowBusinessPage.jsx
@@ -18,6 +18,9 @@ module.exports = React.createClass({
     getInitialState: function () {
         return this.getStateFromStores();
     },
+    onChange: function () {
+        this.setState(this.getStateFromStores());
+    },
     render: function () {
         return <h1>Business Details</h1>;
         var address;
