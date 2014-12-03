50efed4220193af4dc2f0d5c20667bf619270bc4
Antoine Hérault <antoine.herault@gmail.com>
2014-12-03 18:10:55 +0100

    Make timetable edition work

--- actions/Business/Save.js
+++ actions/Business/Save.js
@@ -4,10 +4,10 @@ var hairfieApi = require('../../services/hairfie-api-client');
 var BusinessEvents = require('../../constants/BusinessConstants').Events;
 
 module.exports = function (context, payload, done) {
-    context.dispatch(BusinessEvents.Save);
+    context.dispatch(BusinessEvents.SAVE);
 
     hairfieApi
-        .saveBusiness(payload.values, context.getAuthToken())
+        .saveBusiness(payload.business, context.getAuthToken())
         .then(function (business) {
             context.dispatch(BusinessEvents.SAVE_SUCCESS, {
                 business: business
--- components/BusinessTimetablePage.jsx
+++ components/BusinessTimetablePage.jsx
@@ -5,10 +5,73 @@
 var React = require('react');
 var StoreMixin = require('fluxible-app').StoreMixin;
 var BusinessStore = require('../stores/BusinessStore');
+var BusinessActions = require('../actions/Business');
 var Layout = require('./ProLayout.jsx');
 var _ = require('lodash');
 
 var Weekdays = require('../constants/DateTimeConstants').Weekdays;
+
+var Modal = require('react-bootstrap/Modal');
+var ModalTrigger = require('react-bootstrap/ModalTrigger');
+var Button = require('react-bootstrap/Button');
+
+var TimePicker = React.createClass({
+    render: function () {
+        return <input ref="input" {...this.props} type="text" />
+    },
+    getValue: function () {
+        return this.refs.input.getDOMNode().value;
+    }
+});
+
+var NewTimeWindowModal = React.createClass({
+    render: function () {
+        var dayOfWeekNodes = _.map(_.values(Weekdays), function (wd) {
+            return (
+                <li key={wd}>
+                    <label>
+                        <input type="checkbox" ref={wd} /> {wd}
+                    </label>
+                </li>
+            );
+        }, this);
+
+        return (
+            <Modal {...this.props} title={"Add a time window"}>
+                <div className="modal-body">
+                    <h3>Time window</h3>
+                    From
+                    <TimePicker ref="startTime" />
+                    to
+                    <TimePicker ref="endTime" />
+
+                    <h3>Days of week</h3>
+                    <ul className="list-unstyled">
+                        {dayOfWeekNodes}
+                    </ul>
+                </div>
+                <div className="modal-footer">
+                    <Button onClick={this.save}>Add</Button>
+                </div>
+            </Modal>
+        );
+    },
+    save: function () {
+        this.props.handleSave({
+            timeWindow  : {
+                startTime   : this.refs.startTime.getValue(),
+                endTime     : this.refs.endTime.getValue()
+            },
+            weekdays    : this._getSelectedWeekdays()
+        });
+        this.props.onRequestHide();
+    },
+    _getSelectedWeekdays: function () {
+        return _.filter(_.values(Weekdays), function (wd) {
+            return this.refs[wd].getDOMNode().checked;
+        }, this);
+    }
+});
 
 module.exports = React.createClass({
     mixins: [StoreMixin],
@@ -26,6 +89,8 @@ module.exports = React.createClass({
     render: function () {
         var dayOfWeekRows = _.map(_.values(Weekdays), this.renderWeekday);
 
+        var newTimeWindowModal = <NewTimeWindowModal container={this} handleSave={this.handleSaveNewTimeWindow} />
+
         return (
             <Layout context={this.props.context} business={this.state.business}>
                 <h2>Timetable</h2>
@@ -41,6 +106,10 @@ module.exports = React.createClass({
                         {dayOfWeekRows}
                     </tbody>
                 </table>
+
+                <ModalTrigger modal={newTimeWindowModal} container={this}>
+                    <button onClick={this.openAddForm}>Add time window</button>
+                </ModalTrigger>
             </Layout>
         );
     },
@@ -53,17 +122,17 @@ module.exports = React.createClass({
             <tr key={weekday}>
                 <td>{weekday}</td>
                 <td>
-                    {this.renderTimeWindows(timeWindows)}
+                    {this.renderTimeWindows(weekday, timeWindows)}
                 </td>
             </tr>
         );
     },
-    renderTimeWindows: function (timeWindows) {
+    renderTimeWindows: function (weekday, timeWindows) {
         if (0 == timeWindows.length) {
             return <em>Closed</em>
         }
 
-        var timeWindowNodes = timeWindows.map(this.renderTimeWindow);
+        var timeWindowNodes = timeWindows.map(this.renderTimeWindow.bind(this, weekday));
 
         return (
             <ul className="list-group">
@@ -71,14 +140,49 @@ module.exports = React.createClass({
             </ul>
         );
     },
-    renderTimeWindow: function (timeWindow) {
+    renderTimeWindow: function (weekDay, timeWindow, index) {
+        var start   = timeWindow.startTime,
+            end     = timeWindow.endTime,
+            remove  = this.removeTimeWindow.bind(this, weekDay, start, end);
+
         return (
-            <li className="list-group-item">
+            <li key={index+'-'+start+'-'+end} className="list-group-item">
                 {timeWindow.startTime} - {timeWindow.endTime}
+                <Button className="pull-right" onClick={remove}>
+                    remove
+                </Button>
             </li>
         );
     },
+    handleSaveNewTimeWindow: function (payload) {
+        var timetable = _.cloneDeep(this.state.business.timetable || {});
+        _.map(payload.weekdays, function (wd) {
+            if (!timetable[wd]) timetable[wd] = [payload.timeWindow];
+            else timetable[wd].push(payload.timeWindow);
+        });
+
+        this._saveTimetable(timetable);
+    },
+    removeTimeWindow: function (weekday, startTime, endTime) {
+        console.log('remove', weekday, startTime, endTime);
+        var timetable = _.cloneDeep(this.state.business.timetable || {});
+        if (!timetable[weekday]) return;
+        timetable[weekday] = _.filter(timetable[weekday], function (tw) {
+            return tw.startTime != startTime || tw.endTime != endTime;
+        });
+        this._saveTimetable(timetable);
+    },
     onChange: function () {
         this.setState(this.getStateFromStores());
+    },
+    _saveTimetable: function (timetable) {
+        // TODO: In most cases I prefer to save specific attributes only, should
+        //       we rename the action or change the payload's structure?
+        this.props.context.executeAction(BusinessActions.Save, {
+            business: {
+                id          : this.state.business.id,
+                timetable   : timetable
+            }
+        });
     }
 });
