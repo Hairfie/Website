4b7910a9fa7ad92001b48d57246fc5d1515b979f
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-12-05 15:57:49 +0100

    home pro

--- components/HomePagePro.jsx
+++ components/HomePagePro.jsx
@@ -13,15 +13,15 @@ module.exports = React.createClass({
         return (
             <PublicLayout context={this.props.context} withLogin={true} customClass={'home-pro'}>
                 <div className="row first">
-                    <div className="col-sm-7 col-md-5 col-md-offset-1">
+                    <div className="col-sm-7 col-md-5 col-md-offset-1 left">
                         <h1>Augmentez votre visibilité et votre chiffre d'affaires</h1>
-
-                        <p>
+                        <hr />
+                        <p className="list">
                             <ul>
-                                <li>Gagnez en <strong>visibilité</strong> sur le web et les réseaux sociaux</li>
-                                <li><strong>Différenciez-vous</strong> de vos concurrents</li>
-                                <li>Augmentez votre <strong>chiffre d'affaires</strong></li>
-                                <li>Gagnez du <strong>temps</strong> et concentrez vous sur votre métier</li>
+                                <li><span>Gagnez en <strong>visibilité</strong> sur le web et les réseaux sociaux</span></li>
+                                <li><span><strong>Différenciez-vous</strong> de vos concurrents</span></li>
+                                <li><span>Augmentez votre <strong>chiffre d'affaires</strong></span></li>
+                                <li><span>Gagnez du <strong>temps</strong> et concentrez vous sur votre métier</span></li>
                             </ul>
                         </p>
 
@@ -32,37 +32,39 @@ module.exports = React.createClass({
                         </p>
                     </div>
 
-                    <div className="col-sm-5 col-md-5 col-md-offset-1">
-
-                        <h2>Claim your business</h2>
-                        <div>
-                            <label>
-                                Gender:
-                                <select ref="gender">
-                                    <option value={UserConstants.Genders.MALE}>M</option>
-                                    <option value={UserConstants.Genders.FEMALE}>Mme</option>
-                                </select>
-                            </label>
-                        </div>
-                        <div>
-                            <label>First name: <input ref="firstName" type="text" /></label>
-                        </div>
-                        <div>
-                            <label>Last name: <input ref="lastName" type="text" /></label>
-                        </div>
-                        <div>
-                            <label>Email address: <input ref="email" type="email" /></label>
-                        </div>
-                        <div>
-                            <label>Choose a password: <input ref="password" type="password" /></label>
-                        </div>
-                        <div>
-                            <label>Phone number: <input ref="phoneNumber" type="text" /></label>
-                        </div>
-                        <div>
-                            <label>Company: <input ref="businessName" type="text" /></label>
-                        </div>
-                        <button onClick={this.submit}>Submit</button>
+                    <div className="col-sm-5 col-md-4 col-md-offset-1">
+                        <form role="form" className="claim">
+                            <h3>Vous êtes un <strong>professionnel</strong> de la coiffure ?</h3>
+                            <div className="form-group radio">
+                                <label className="radio-inline">
+                                  <input type="radio" name="gender" ref="gender" value={UserConstants.Genders.MALE} />
+                                  Man
+                                </label>
+                                <label className="radio-inline">
+                                  <input type="radio" name="gender" ref="gender" value={UserConstants.Genders.FEMALE} />
+                                  Woman
+                                </label>
+                            </div>
+                           <div className="form-group">
+                                <input ref="businessName" type="text" className="form-control" placeholder="Business Name"/>
+                            </div>
+                            <div className="form-group">
+                                <input ref="firstName" type="text" className="form-control" placeholder="First Name"/>
+                            </div>
+                            <div className="form-group">
+                                <input ref="lastName" type="text" className="form-control" placeholder="Last Name"/>
+                            </div>
+                            <div className="form-group">
+                                <input ref="email" type="email" className="form-control" placeholder="Email"/>
+                            </div>
+                            <div className="form-group">
+                                <input ref="password" type="password" className="form-control" placeholder="Choose a password"/>
+                            </div>
+                            <div className="form-group">
+                                <input ref="phoneNumber" type="text" className="form-control" placeholder="Phone Number"/>
+                            </div>
+                            <button type="button" className="btn btn-red btn-block" onClick={this.submit}>Start now !</button>
+                        </form>
                     </div>
                 </div>
             </PublicLayout>
