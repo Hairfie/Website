458e79bdadd167dbbd8def5eca4c6ca44573d19a
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-12-15 19:59:38 +0100

    staging ?

--- configs/hairfie-api.js
+++ configs/hairfie-api.js
@@ -5,10 +5,9 @@ var resolve = require('../lib/config').resolve;
 module.exports = resolve({
     namespace:  'HAIRFIE_API',
     defaults: {
-        //URL: 'http://api-staging.hairfie.com/api'
-        URL: 'http://localhost:3000/api'
+        URL: 'http://api-staging.hairfie.com/api'
     },
     production: {
-        URL: 'http://api.hairfie.com/api'
+        URL: 'http://api-staging.hairfie.com/api'
     }
 });
