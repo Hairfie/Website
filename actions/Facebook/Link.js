919f20642633a4bfc737a0617e1d430abcfb8f28
Antoine Hérault <antoine.herault@gmail.com>
2014-12-18 15:10:03 +0100

    Refresh login status on link

--- actions/Facebook/Link.js
+++ actions/Facebook/Link.js
@@ -2,21 +2,29 @@
 
 var hairfieApi = require('../../services/hairfie-api-client');
 var Facebook = require('../../services/facebook');
+var FacebookEvents = require('../../constants/FacebookConstants').Events;
 var login = require('./utils').login;
+var RefreshPermissions = require('./RefreshPermissions');
 
 module.exports =  function (context, payload, done) {
     Facebook
         .load()
         .then(function (fb) {
-            return login(fb, payload.scope);
+            return login(fb, payload.scope)
+                .then(function(token) {
+                    // dispatch status to refresh permissions
+                    // TODO: dispatch a dedicated event?
+                    fb.getLoginStatus(function (loginStatus) {
+                        context.dispatch(FacebookEvents.RECEIVE_LOGIN_STATUS, {
+                            loginStatus: loginStatus
+                        });
+                    });
+
+                    return token;
+                });
         })
         .then(function (token) {
             return hairfieApi.linkFacebookToken(token, context.getAuthToken());
         })
-        .then(function () {
-            done();
-        })
-        .fail(function (error) {
-            done(error);
-        });
+        .then(done.bind(null, null), done);
 };
