b9a9c3430783686b4928338c3715bd72e5b80e80
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-12-22 18:00:07 +0100

    fix bad jsx

--- components/BusinessCustomersPage.jsx
+++ components/BusinessCustomersPage.jsx
@@ -50,7 +50,7 @@ module.exports = React.createClass({
             <Layout context={this.props.context} business={this.state.business}>
                 <h2>Clients</h2>
                 <div className="alert alert-success">
-                    <strong>Attention, pour remplir cette page pensez à prendre systématiquement les emails de vos clients au moment de poster un hairfie !
+                    <strong>Attention, pour remplir cette page</strong> pensez à prendre systématiquement les emails de vos clients au moment de poster un hairfie !
                 </div>
                 <table className="table table-bordered">
                     <thead>
