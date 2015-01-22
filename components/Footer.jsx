f42edc260f966721dcb17ff36abf0752d254c3cd
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-01-22 10:19:18 +0100

    fix footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,6 +14,7 @@ module.exports = React.createClass({
                     <a className="social" href="https://instagram.com/HairfieApp" target="_blank"><img src="/img/instagram@2x.png" /></a>
                     <a className="social" href="https://twitter.com/HairfieApp" target="_blank"><img src="/img/twitter@2x.png" /></a>
                     <a className="social" href="mailto:hello@hairfie.com" target="_blank"><img src="/img/mail@2x.png" /></a>
+                    <a className="social" href="https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=853590611&mt=8" target="_blank"><img src="/img/btn-apple@2x.png" /></a>
                 </div>
             </footer>
         );
