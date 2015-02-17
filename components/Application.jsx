bd7232198f9c52d846dbf76839247373b492a6c3
Antoine Hérault <antoine.herault@gmail.com>
2015-02-17 16:38:02 +0100

    Fix access control

--- components/Application.jsx
+++ components/Application.jsx
@@ -4,20 +4,23 @@ var React = require('react');
 var StoreMixin = require('fluxible').StoreMixin;
 var RouterMixin = require('flux-router-component').RouterMixin;
 var RouteStore = require('../stores/RouteStore');
+var AuthStore = require('../stores/AuthStore');
 
 var NotFoundPage = require('./NotFoundPage.jsx');
 var BusinessInfosPage = require('./BusinessInfosPage.jsx');
+var Navigate = require('flux-router-component/actions/navigate');
 
 var ga = require('../services/analytics');
 
 module.exports = React.createClass({
     mixins: [StoreMixin, RouterMixin],
     statics: {
-        storeListeners: [RouteStore]
+        storeListeners: [RouteStore, AuthStore]
     },
     getStateFromStores: function () {
         return {
-            route: this.getStore(RouteStore).getCurrentRoute()
+            route: this.getStore(RouteStore).getCurrentRoute(),
+            isAuthenticated: this.getStore(AuthStore).isAuthenticated()
         };
     },
     getInitialState: function () {
@@ -26,6 +29,22 @@ module.exports = React.createClass({
     render: function () {
         var route = this.state.route;
         if (route && route.config && route.config.pageComponent) {
+            console.log(route.config, this.state.isAuthenticated);
+            // check access is granted
+            if (route.config.authRequired && !this.state.isAuthenticated) {
+                this.props.context.executeAction(Navigate, {
+                    url: this.props.context.makePath('pro_home')
+                });
+
+                return <p>Access denied</p>;
+            }
+
+            if (route.config.leaveAfterAuth && this.state.isAuthenticated) {
+                this.props.context.executeAction(Navigate, {
+                    url: this.props.context.makePath('pro_dashboard')
+                });
+            }
+
             ga('send', 'pageview', {
                 'page': route.url
             });
--- stores/RouteStore.js
+++ stores/RouteStore.js
@@ -2,18 +2,13 @@
 
 var createStore = require('fluxible/utils/createStore');
 var makeHandlers = require('../lib/fluxible/makeHandlers');
-var AuthStore = require('./AuthStore');
-var AuthEvents = require('../constants/AuthConstants').Events;
 var debug = require('debug')('App:RouteStore');
 var _ = require('lodash');
-
-var navigateAction = require('flux-router-component/actions/navigate');
 
 module.exports = createStore({
     storeName: 'RouteStore',
     handlers: makeHandlers({
-        handleChangeRouteSuccess: 'CHANGE_ROUTE_SUCCESS',
-        handleAuthChange: [AuthEvents.LOGIN_SUCCESS, AuthEvents.LOGOUT_SUCCESS]
+        handleChangeRouteSuccess: 'CHANGE_ROUTE_SUCCESS'
     }),
     initialize: function () {
         this.currentRoute = null;
@@ -39,40 +34,6 @@ module.exports = createStore({
         this.currentRoute = route;
 
         this.emitChange();
-
-        // should not be the responsability of the auth store
-        this.applyAuthRules();
-    },
-    handleAuthChange: function (payload) {
-        this.dispatcher.waitFor([AuthStore], this.applyAuthRules.bind(this, false));
-    },
-    applyAuthRules: function (alwaysEmitChange) {
-        var isAuthenticated = !!this.dispatcher.getStore(AuthStore).getUser();
-
-        if (isAuthenticated && this.currentRoute && this.currentRoute.config.leaveAfterAuth) {
-            debug('user is authenticated, redirecting user to after login page');
-            this.getContext().executeAction(navigateAction, {
-                url: this.getContext().makePath('pro_dashboard')
-            }, function () {});
-        }
-
-        if (!isAuthenticated && this.currentRoute && this.currentRoute.config.authRequired) {
-            debug('user is not authenticated, redirecting user to login page');
-            this.getContext().executeAction(navigateAction, {
-                url: this.getContext().makePath('pro_home')
-            }, function () {});
-        }
-    },
-    navigateTo: function (routeName, params) {
-        var url = routes[routeName].path;
-
-        _.forIn(params, function (value, param) {
-            url = url.replace(param, value);
-        });
-
-        this.currentRouteName = routeName;
-        this.currentUrl = url;
-        this.currentParams = params || {};
     },
     getCurrentRoute: function () {
         return this.currentRoute;
