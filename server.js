a6a35dc8cee335ed8ef26398d8d97079b4bf0106
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-03-04 11:54:22 +0100

    add new relci

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require('node-jsx').install({extension:'.jsx'});
 
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+    require('newrelic');
+}
+
 var express         = require('express');
 var path            = require('path');
 var favicon         = require('serve-favicon');
