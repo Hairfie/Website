2a466253ed4e65078f98449d13ba57a10cac0d20
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-03-30 09:13:01 +0200

    fix homepage

--- components/HomePage/HomePageMixin.jsx
+++ components/HomePage/HomePageMixin.jsx
@@ -3,34 +3,35 @@
 var HomePageMixin = {
     componentDidMount: function () {
         TweenMax.to('.landing header.hidden-xs', 1, {
-          opacity:1,
-          ease:Power4.easeInOut
+            opacity:1,
+            ease:Power4.easeInOut
         });
         TweenMax.to('.headline', 0.7, {
-          opacity:1,
-          top:0,
-          ease:Power4.easeInut,
-          delay:0.5
+            opacity:1,
+            top:0,
+            ease:Power4.easeInut,
+            delay:0.5
         });
         TweenMax.to('.searchbar.hidden-xs', 0.7, {
-          opacity:1,
-          marginTop:'+=20',
-          ease:Power4.easeOut,
-          delay:0.65
+            opacity:1,
+            marginTop:'+=20',
+            ease:Power4.easeOut,
+            delay:0.65
         });
 
         $('body').on("click",'.mobile-nav .menu-trigger',function(){
-          if( $('.mobile-menu').height() == 0 ) {
-            $('.menu-trigger').addClass('close');
-            TweenMax.to('.mobile-menu', 0.4, {height:'100vh',ease:Power2.easeInOut});
-          } else {
-            $('.menu-trigger').removeClass('close');
-            TweenMax.to('.mobile-menu', 0.4, {height:0,ease:Power2.easeOut});
-          }
+            if( $('.mobile-menu').height() == 0 ) {
+                $('body').toggleClass('locked');
+                $('.menu-trigger').addClass('close');
+                TweenMax.to('.mobile-menu', 0.4, {height:'100vh',ease:Power2.easeInOut});
+            } else {
+                $('body').toggleClass('locked');
+                $('.menu-trigger').removeClass('close');
+                TweenMax.to('.mobile-menu', 0.4, {height:0,ease:Power2.easeOut});
+            }
         });
-
         $('body').on('click','.main-searchbar input', function(){
-          $('.main-searchbar').toggleClass('open');
+            $('.main-searchbar').toggleClass('open');
         });
     }
 };
