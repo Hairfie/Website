99d82ed87d6a549601db576280559782da7d3f40
Bastien Chevallier <bastien@hairfie.com>
2015-07-24 14:47:03 +0200

    rst password update

--- components/Auth/FormConnect.jsx
+++ components/Auth/FormConnect.jsx
@@ -3,42 +3,38 @@
 var React = require('react');
 var _ = require('lodash');
 var AuthActions = require('../../actions/AuthActions');
+var NavigationActions = require('../../actions/NavigationActions');
 var Input = require('react-bootstrap/Input');
+var Link = require('../Link.jsx');
 
 module.exports = React.createClass({
 	contextTypes: {
         executeAction: React.PropTypes.func
-    },
-    getInitialState: function () {
-        return {reset_pwd: false}
     },
 	render: function() {
 		return (
         <form className="form">
             <Input type="email" ref="email" placeholder="Adresse Email *"/>
             <Input type="password" ref="password" placeholder="Mot de Passe *" />
-            <a role="button" onClick={this.handleResetPasswordChanged} className="green">Mot de passe oublié ?</a>
-            {this.renderResetPassword()}
+            <a role="button" onClick={this.resetPassword} className="green">Mot de passe oublié ?</a>
             <a role="button" onClick={this.submit} className="btn btn-red full">Se connecter</a>
         </form>
 		);
 	},
-    renderResetPassword: function() {
-        if (!this.state.reset_pwd) return;
-            return (<p className="red">Veuillez indiquer votre email dans le champ email et recliquer sur "Mot de passe oublié ?"</p>);
+    resetPassword: function(e) {
+        e.preventDefault();
+        return this.context.executeAction(NavigationActions.navigate, {
+            route: 'ask_reset_password',
+            query: { email: this.refs.email.getValue() }
+        });
     },
 	submit: function() {
         var email = this.refs.email.getValue();
         var password = this.refs.password.getValue();
-        this.context.executeAction(AuthActions.emailConnect, { email: email, password: password, withNavigate: this.props.withNavigate });
-	},
-    handleResetPasswordChanged: function() {
-        var email = this.refs.email.getValue();
-        if (email) {
-            this.context.executeAction(AuthActions.askResetPassword, {email: email});
-            this.setState({reset_pwd: false});
-            return;
-        }
-        this.setState({reset_pwd: true});
-    }
+        this.context.executeAction(AuthActions.emailConnect, {
+            email: email,
+            password: password,
+            withNavigate: this.props.withNavigate
+        });
+	}
 });
