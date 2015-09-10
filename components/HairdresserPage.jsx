11f3f78159625de2e67acef229108ac826f7c2e8
Bastien Chevallier <bastien@hairfie.com>
2015-09-10 14:59:35 +0200

    fix responsive

--- components/HairdresserPage.jsx
+++ components/HairdresserPage.jsx
@@ -30,7 +30,7 @@ var HairdresserPage = React.createClass({
                         />
                     </Link>
                 </div>
-                <div className="col-xs-9 col-md-10">
+                <div className="col-xs-9 col-lg-10">
                     <div>
                         <Link route="business" params={{ businessId: business.id, businessSlug: business.slug }}><h5>{business.name}</h5></Link>
                         <p>{address.street} {address.zipCode} {address.city}</p>
