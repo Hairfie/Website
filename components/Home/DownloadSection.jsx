3d25daffc15560ad7450a807ce4fca45daf363c9
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-09-17 18:43:27 +0200

    fix

--- components/Home/DownloadSection.jsx
+++ components/Home/DownloadSection.jsx
@@ -10,7 +10,7 @@ module.exports = React.createClass({
                 <div className="container">
                     <div className="col-sm-6">
                         <h2>Pour poster un Hairfie, téléchargez notre application</h2>
-                        <p>Pour avoir toujours Hairfie au bout des doigts et pouvoir poster vos plus beaux #Hairfies, téléchargez notre application iOS pour le moment (Android bientôt)</p>
+                        <p>Pour avoir toujours Hairfie au bout des doigts et pouvoir poster vos plus beaux #Hairfies, téléchargez notre application iOS et bientôt Android</p>
                         <p>
                             <a href="https://itunes.apple.com/fr/app/hairfie/id853590611?mt=8" className="btn-apple center-block" target="_blank">
                                 <Picture picture={{url: '/images/btn-apple@2x.png'}} style={{width: 204, height: 64}}/>
