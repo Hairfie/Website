046d02ecb1cdcc99e20af736b702a42c691ed114
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-10-15 12:07:38 +0200

    fix

--- stores/HomeLinkStore.js
+++ stores/HomeLinkStore.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var createStore = require('fluxible/addons/createStore');
-var homeLinks = require('../constants/links').homeLinks;
+var homeLinks = require('../constants/Links').homeLinks;
 
 module.exports = createStore({
     storeName: 'HomeLinkStore',
