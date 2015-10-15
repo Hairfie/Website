54b49bee80e6e44240594a4652ec778985df20f5
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-10-15 12:10:29 +0200

    fix

--- actions/BlogPostActions.js
+++ actions/BlogPostActions.js
@@ -5,7 +5,7 @@ var Actions = require('../constants/Actions');
 module.exports = {
     getRecent: function (context) {
         return context.hairfieApi
-            .get('/blogPosts', { query: { limit: 2 } })
+            .get('/blogPosts', { query: { limit: 3 } })
             .then(function (posts) {
                 context.dispatch(Actions.RECEIVE_BLOG_POSTS, posts);
             });
