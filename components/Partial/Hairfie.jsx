43ee6d8964453ffeb2f8480203d4aba9936aef3e
Bastien Chevallier <bastien@hairfie.com>
2015-10-28 17:00:04 +0100

    fix

--- components/Partial/Hairfie.jsx
+++ components/Partial/Hairfie.jsx
@@ -5,7 +5,7 @@ var _ = require('lodash');
 var Picture = require('./Picture.jsx');
 var Link = require('../Link.jsx');
 
-function displayName(u) { var u = u || {}; return u.firstName+' '+(u.lastName || '').substr(0, 1); }
+function displayName(u) { var u = u || {}; return u.firstName; }
 
 module.exports = React.createClass({
     render: function () {
@@ -13,11 +13,11 @@ module.exports = React.createClass({
 
         var hairdresser = <p></p>;
         if (hairfie.hairdresser) {
-            hairdresser = <p><span className="underline">Coiffeur</span>: {displayName(hairfie.hairdresser)}</p>;
+            hairdresser = <p><span className="underline">Coiffeur</span> : {displayName(hairfie.hairdresser)}</p>;
         }
         var salon = <p></p>;
         if (hairfie.business && hairfie.business.name) {
-            salon = <p><span className="underline">Salon</span>: {hairfie.business.name}</p>;
+            salon = <p><span className="underline">Salon</span> : {hairfie.business.name}</p>;
         }
 
         var price;
@@ -33,8 +33,8 @@ module.exports = React.createClass({
                                 placeholder="/img/placeholder-640.png"
                                 alt="" />
                             <figcaption>
-                                {hairdresser}
                                 {salon}
+                                {hairdresser}
                                 {price}
                                 {hairfie.pictures.length > 1 ? <Picture picture={_.first(hairfie.pictures)} style={{position: 'absolute', width:'40%', top: '0px', right: '0px'}} /> : null}
                             </figcaption>
