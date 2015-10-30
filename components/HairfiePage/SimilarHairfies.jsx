b606142200a2450f80e2fd99288be91a947b51cb
Bastien Chevallier <bastien@hairfie.com>
2015-10-30 10:31:48 +0100

    similar

--- components/HairfiePage/SimilarHairfies.jsx
+++ components/HairfiePage/SimilarHairfies.jsx
@@ -2,17 +2,10 @@
 
 var React = require('react');
 var _ = require('lodash');
-var Picture = require('../Partial/Picture.jsx');
-var Link = require('../Link.jsx');
 var HairfieActions = require('../../actions/HairfieActions');
-
-var moment = require('moment');
-require('moment/locale/fr');
-moment.locale('fr');
+var Hairfie = require('../Partial/Hairfie.jsx');
 
 var PAGE_SIZE = 12;
-
-function displayName(u) { var u = u || {}; return u.firstName+' '+(u.lastName || '').substr(0, 1); }
 
 module.exports = React.createClass({
     contextTypes: {
@@ -27,33 +20,8 @@ module.exports = React.createClass({
                 <div className="row">
                     {this.props.similarHairfies.length > 0 ? <h3 className="col-xs-12 text-center">Hairfies Similaires</h3> : ''}
                     {_.map(this.props.similarHairfies, function (hairfie) {
-                        var hairdresser = <p>&nbsp;</p>;
-                        if (hairfie.hairdresser) {
-                            hairdresser = <p>Coiffé par <span>{displayName(hairfie.hairdresser)}</span></p>;
-                        }
-                        var price;
-                        if (hairfie.price) {
-                            price = <div className="pricetag">{hairfie.price.amount}€</div>;
-                        }
-
-                        return (
-                            <div key={hairfie.id} className="col-xs-6 col-sm-3 col-lg-2 single-hairfie">
-                                <figure>
-                                    <Link route="hairfie" params={{ hairfieId: hairfie.id }}>
-                                        <Picture picture={_.last(hairfie.pictures)}
-                                                resolution={{width: 640, height: 640}}
-                                                placeholder="/img/placeholder-640.png"
-                                                alt="" />
-                                        <figcaption>
-                                            {hairdresser}
-                                            <p><span>Le {moment(hairfie.createdAt).format('L')}</span></p>
-                                            {price}
-                                        </figcaption>
-                                    </Link>
-                                </figure>
-                            </div>
-                        );
-                    }, this)}
+                        return <Hairfie hairfie={hairfie} className="col-xs-6 col-sm-3 col-lg-2 single-hairfie" />;
+                    })}
                 </div>
                 {this.renderMoreButton()}
             </div>
