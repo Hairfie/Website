ad552e7c7f4fbc8f20258f123133fd9343ffa1c2
Ghislain de Juvigny <gdjuvigny@gmail.com>
2015-12-08 09:58:26 +0100

    fix

--- components/Home/HomePageMixin.jsx
+++ components/Home/HomePageMixin.jsx
@@ -3,25 +3,25 @@
 var HomePageMixin = {
     componentDidMount: function () {
         // animation
-        TweenMax.set('.headline', {opacity:0,top:30});
-        TweenMax.set('.searchbar.hidden-xs', {opacity:0,marginTop:'-=20'});
-        TweenMax.set('.landing header.hidden-xs', {opacity:0});
-        TweenMax.to('.landing header.hidden-xs', 1, {
-            opacity:1,
-            ease:Power4.easeInOut
-        });
-        TweenMax.to('.headline', 0.7, {
-            opacity:1,
-            top:0,
-            ease:Power4.easeInut,
-            delay:0.5
-        });
-        TweenMax.to('.searchbar.hidden-xs', 0.7, {
-            opacity:1,
-            marginTop:'+=20',
-            ease:Power4.easeOut,
-            delay:0.65
-        });
+        // TweenMax.set('.headline', {opacity:0,top:30});
+        // TweenMax.set('.searchbar.hidden-xs', {opacity:0,marginTop:'-=20'});
+        // TweenMax.set('.landing header.hidden-xs', {opacity:0});
+        // TweenMax.to('.landing header.hidden-xs', 1, {
+        //     opacity:1,
+        //     ease:Power4.easeInOut
+        // });
+        // TweenMax.to('.headline', 0.7, {
+        //     opacity:1,
+        //     top:0,
+        //     ease:Power4.easeInut,
+        //     delay:0.5
+        // });
+        // TweenMax.to('.searchbar.hidden-xs', 0.7, {
+        //     opacity:1,
+        //     marginTop:'+=20',
+        //     ease:Power4.easeOut,
+        //     delay:0.65
+        // });
 
         // mobile
         $('body').on("click",'.mobile-nav .menu-trigger',function(){
