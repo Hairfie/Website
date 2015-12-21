96e832763ccf2330b0e5b5682d566581251ade53
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2015-12-21 11:26:40 +0100

    some fix

--- components/BusinessPage/ShortInfos.jsx
+++ components/BusinessPage/ShortInfos.jsx
@@ -67,7 +67,7 @@ module.exports = React.createClass({
                   </div>
                   {this.renderAveragePrice()}
                 </div>
-                <div className="col-sm-4" style={{padding: '0', paddingRight: '15px', marginTop: '-5px'}}>
+                <div className="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-4" style={{padding: '0', paddingRight: '15px', marginTop: '-5px'}}>
                   <Link className="btn" route="write_business_review" query={{businessId: this.props.business.id}}>DÉPOSEZ UN AVIS</Link>
                   <div className="hidden-xs">
                     {this.renderTimetable()}
