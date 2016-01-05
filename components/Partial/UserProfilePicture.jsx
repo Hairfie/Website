5c2a9a2b06703590870cf6e5a810a1db2e881887
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2016-01-05 18:15:06 +0100

    in progress

--- components/Partial/UserProfilePicture.jsx
+++ components/Partial/UserProfilePicture.jsx
@@ -4,15 +4,17 @@ var React = require('react');
 var Picture = require('./Picture.jsx');
 var UserGenders = require('../../constants/UserConstants').Genders;
 
-var SRC_DEFAULT_MAN = '/img/profile-picture/default-man.png';
-var SRC_DEFAULT_WOMAN = '/img/profile-picture/default-woman.png';
+var SRC_DEFAULT_MAN = '/img/profile-picture/default-man.svg';
+var SRC_DEFAULT_WOMAN = '/img/profile-picture/default-woman.svg';
 
 module.exports = React.createClass({
     render: function () {
         var gender    = this.props.gender || {},
-            picture = this.props.picture;
+            picture = this.props.picture,
+            svg = false;
 
         if (!picture) {
+            svg = true;
             switch (gender) {
                 case 'MALE':
                     picture = {url: SRC_DEFAULT_MAN};
@@ -23,6 +25,6 @@ module.exports = React.createClass({
             }
         }
 
-        return <Picture {...this.props} picture={picture} />
+        return <Picture {...this.props} picture={picture} svg={svg} />
     }
 });
