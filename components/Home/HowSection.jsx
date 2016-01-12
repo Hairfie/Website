639227851e964e8ad4abe0ab9c60dccd81c99bdb
Taskim <miksa_t@etna-alternance.net>
2016-01-12 18:22:55 +0100

    size howsection

--- components/Home/HowSection.jsx
+++ components/Home/HowSection.jsx
@@ -11,17 +11,17 @@ module.exports = React.createClass({
                 <p className="subtitle" />
                 <div className="row">
                     <div className="col-sm-4 col-xs-12">
-                        <Picture picture={{url: "/img/icons/search.svg"}} svg={true} alt="Trouvez votre coupe" />
+                        <Picture picture={{url: "/img/icons/search.svg"}} svg={true} style={{width: 64, height: 64}} alt="Trouvez votre coupe" />
                         <h3>S’inspirer</h3>
                         <p>des hairfies postés et trouver son coiffeur</p>
                     </div>
                     <div className="col-sm-4 col-xs-12">
-                        <Picture picture={{url: "/img/icons/calendar.svg"}} svg={true} alt="Prenez RDV avec votre coiffeur" />
+                        <Picture picture={{url: "/img/icons/calendar.svg"}} svg={true} style={{width: 64, height: 64}} alt="Prenez RDV avec votre coiffeur" />
                         <h3>Prendre RDV</h3>
                         <p>24/7 gratuitement sans paiement en ligne</p>
                     </div>
                     <div className="col-sm-4 col-xs-12">
-                        <Picture picture={{url: "/img/icons/heart.svg"}} svg={true} alt="Partagez votre #Hairfie" />
+                        <Picture picture={{url: "/img/icons/heart.svg"}} svg={true} style={{width: 64, height: 64}} alt="Partagez votre #Hairfie" />
                         <h3>Partager</h3>
                         <p>son expérience.<br />Poster un hairfie et un avis.</p>
                     </div>
