7a14f1a83b1d5f93df767cbfeeab2bd408add0dd
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2016-01-28 16:15:17 +0100

    fix

--- actions/NavigationActions.js
+++ actions/NavigationActions.js
@@ -9,7 +9,6 @@ module.exports = {
         if (params.noNav) {
         	return window.history.pushState("", "", url);
         }
-
         return context.executeAction(navigateAction, { url: url });
     }
 };
