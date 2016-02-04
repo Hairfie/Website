e94e902fb81d99be97dd00578999e02a57a4857a
Taskim <miksa_t@etna-alternance.net>
2016-02-04 09:48:20 +0100

    how section

--- components/Home/HowSection.jsx
+++ components/Home/HowSection.jsx
@@ -8,22 +8,22 @@ module.exports = React.createClass({
         return (
             <section className="home-section how">
                 <div className="row">
-                    <h2 className="col-sm-3">Hairfie comment ça marche&nbsp;?</h2>
-                    <div className="col-sm-3 col-xs-12">
+                    <h2 className="col-sm-3 col-xs-12">Hairfie comment ça marche&nbsp;?</h2>
+                    <div className="col-sm-3 col-sm-offset-0 col-xs-9 col-xs-offset-2">
                         <Picture picture={{url: "/img/home/photo-red.svg"}} svg={true} style={{width: 40, height: 100, float: 'left'}} alt="Trouvez votre coupe" />
                         <div className="how-txt">
                             <h3>S’inspirer</h3>
                             <p>des hairfies postés et trouver son coiffeur</p>
                         </div>
                     </div>
-                    <div className="col-sm-3 col-xs-12">
+                    <div className="col-sm-3 col-sm-offset-0 col-xs-9 col-xs-offset-2">
                         <Picture picture={{url: "/img/icons/calendar.svg"}} svg={true} style={{width: 40, height: 100, float: 'left'}} alt="Prenez RDV avec votre coiffeur" />
                         <div className="how-txt">
                             <h3>Prendre RDV</h3>
                             <p>24/7 gratuitement sans paiement en ligne</p>
                         </div>
                     </div>
-                    <div className="col-sm-3 col-xs-12">
+                    <div className="col-sm-3 col-sm-offset-0 col-xs-9 col-xs-offset-2">
                         <Picture picture={{url: "/img/icons/heart.svg"}} svg={true} style={{width: 40, height: 100, float: 'left'}} alt="Partagez votre #Hairfie" />
                         <div className="how-txt">
                             <h3>Partager</h3>
