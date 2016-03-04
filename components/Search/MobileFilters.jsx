5dbc9f545bd431bf3563cf79c176bad3f5111353
Ghislain de Juvigny <gdjuvigny@gmail.com>
2016-03-04 18:12:06 +0100

    fix pricelevel mobile

--- components/Search/MobileFilters.jsx
+++ components/Search/MobileFilters.jsx
@@ -10,6 +10,7 @@ var TagSubFilters = require('./TagSubFilters.jsx');
 var CategorySubFilters = require('./CategorySubFilters.jsx');
 var PromoCheckbox = require('./PromoCheckbox.jsx');
 var OpeningDays = require('./OpeningDays.jsx');
+var PriceFilterMobile = require('./PriceFilterMobile.jsx');
 var Selections = require('./Selections.jsx');
 var BusinessNameInput = require('./BusinessNameInput.jsx');
 var LocationInput = require('./LocationInput.jsx');
@@ -75,6 +76,10 @@ var MobileFilters = React.createClass({
                     initialSearch={this.state.search}
                     cat={this.state.filtersCategoryToDisplay}
                     onClose={this.handleCloseMobileSubFilters} />
+                <PriceFilterMobile
+                    initialSearch={this.state.search}
+                    cat={this.state.filtersCategoryToDisplay}
+                    onClose={this.handleCloseMobileSubFilters} />
                 <div className="filter-header">Filtrer par:</div>
                 <div>
                     <LocationInput 
@@ -108,6 +113,11 @@ var MobileFilters = React.createClass({
                         Nos sélections de coiffeurs {this.countCategories(this.state.search.selections)}
                     </a>
                 </Selections>
+                <div>
+                    <a role="button" className="filters-category" onClick={this.handleDisplayMobileSubFilters.bind(this, 'PriceFilterMobile')}>
+                        Prix {this.countCategories(this.state.search.priceLevel)}
+                    </a>
+                </div>
                 <div>
                     <PromoCheckbox
                         ref="promoCheckbox"
