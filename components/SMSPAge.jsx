caef78d07a06d8bf573b9ee80025d3cde4199f1a
Taskim <miksa_t@etna-alternance.net>
2016-03-25 09:08:34 +0100

    f

--- components/SMSPAge.jsx
+++ components/SMSPAge.jsx
@@ -13,10 +13,11 @@ var NewsletterPage = React.createClass({
                         <div className="row">
                             <div className="col-xs-12 flex-container">
                                 <h3>Nous contacter gratuitement par SMS</h3>
+                                <hr className="hr" />
                                 <p>"Salut Hairfie, je voudrais aller chez le coiffeur aujourd'hui. Peux-tu m'aider ?"</p>
-                                <p>"Salut Hairfie, je cherche le meilleur spécialiste du balayage à Paris ! Peux tu m'aider ?"</p>
-                                <p>"Salut Hairfie, j'ai un énorme problème avec mes cheveux ! On a complétement raté ma coloration... Je ne sais pas quoi faire pour la rattraper. Besoin de conseils !!"</p>
-                                <a href="sms:/0644600250">0644600250</a>
+                                <p>"Bonjour Hairfie, je cherche le meilleur spécialiste du balayage à Paris ! Peux tu m'aider ?"</p>
+                                <p>"Hello Hairfie, j'ai un énorme problème avec mes cheveux ! On a complétement raté ma coloration... Je ne sais pas quoi faire pour la rattraper. Besoin de conseils !!"</p>
+                                <a href="sms:+33644600250" className="btn btn-book">0644600250</a>
                             </div>
                         </div>
                     </div>
